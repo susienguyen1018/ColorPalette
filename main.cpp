d185505708da613e6644dc05ec86e9904dececc1
Susie Nguyen <snguyen8@csuchico.edu>
2024-02-21 11:20:15 -0800

    Improve error messaging #1

--- main.cpp
+++ main.cpp
@@ -6,28 +6,37 @@ using namespace std;
 bool all_hex(char*);
 
 int main(int argc, char* argv[]) {
-  int valid_colors = 0;
+    if (argc < 2) {
+        cout << "ERR_MISSING: One or more RGB values should be provided as arguments, separated by spaces" << endl;
+        return 1;
+    }
 
-  for(int i=1; i < argc; i++) {
-    int length = strlen(argv[i]);
-    if( (length == 3 || length == 6) && all_hex(argv[i]) ) {
-      cout << "#" << argv[i] << endl;
-    }  
-  }
-  return 0;
+    for(int i = 1; i < argc; i++) {
+        int length = strlen(argv[i]);
+        bool isHex = all_hex(argv[i]);
+
+        if ((length == 3 || length == 6) && isHex) {
+            cout << "#" << argv[i] << endl;
+        } else {
+            if (length != 3 && length != 6) {
+                cout << argv[i] << " ERR_LENGTH: RGB values must be either 3 or 6 characters long" << endl;
+            }
+            if (!isHex) {
+                cout << argv[i] << " ERR_VALUE: RGB values must be in 0-F range" << endl;
+            }
+        }
+    }
+    return 0;
 }
 
 bool all_hex(char* word) {
-  for(int i=0; i<strlen(word); i++) {
-    if((word[i] >= '0' && word[i] <= '9') || (word[i] >= 'a' && word[i] <= 'f')
-      || (word[i] >= 'A' && word[i] <='F')) {
-      continue;
+    for(int i = 0; i < strlen(word); i++) {
+        if((word[i] >= '0' && word[i] <= '9') || (word[i] >= 'a' && word[i] <= 'f') || (word[i] >= 'A' && word[i] <= 'F')) {
+            continue;
+        } else {
+            return false;
+        }
     }
-    else {
-      return false;
-    }
-  }
-
-  //no non-hex characters found
-  return true;
+    // No non-hex characters found
+    return true;
 }
